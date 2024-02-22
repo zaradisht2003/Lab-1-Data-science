2dfbb0d664c994ad784911802f02b9b280dd1023
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2024-02-22 21:12:49 +0200

    added age and faculty

--- Program.py
+++ Program.py
@@ -4,10 +4,14 @@ lis=[]
 for i in range(num):
     name=input("Enter the name of student #"+str(i+1)+": ")
     id=input("Enter the ID of student #"+str(i+1)+": ")
-    lis.append([name,id])
+    age=input("Enter the Age of student #"+str(i+1)+": ")
+    faculty=input("Enter the Faculty of student #"+str(i+1)+": ")
+    lis.append([name,id,age,faculty])
 
 for i in range(num):
     print("Student #",i+1,": ")
     print("  Name: ",lis[i][0])
     print("  ID:   ",lis[i][1])
+    print("  Age:   ",lis[i][2])
+    print("  Faculty:   ",lis[i][3])
 
